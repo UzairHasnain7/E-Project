c41dbf1d1a1a82cbb0077e55ede6c1e8853f728c
UzairHasnain7 <uzairhasnain@gmail.com>
2023-05-04 14:21:15 -0700

    E-Project

---
+++ assets/image/app.js
@@ -0,0 +1,9 @@
+alert("Welcome Wedding Planners")
+
+// var num1 = parseInt(prompt("Please enter your first number"));
+// var num12 = parseInt(prompt("Please enter your second number"));
+
+// var password = num1 + num2;
+// document.write("first number is : " + num1 + "<br/>");
+// document.write("first number is : " + num2 + "<br/>");
+// document.write("The sum of two number is : " + password + "<br/>");
